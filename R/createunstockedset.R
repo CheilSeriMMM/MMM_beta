7f497f0f537f3aa9f31e9ae6ed6e6f7e69215c94
SM Ryu <seungme.ryu@samsung.com>
2017-04-06 15:29:47 +0900

    Update createunstockedset.R

--- R/createunstockedset.R
+++ R/createunstockedset.R
@@ -1,3 +1,25 @@
 createunstockedset=function(){
+  log_data=unstockeddata[,]
+  
+  logadj<-5
+  ration_adj<-0.00001
+  
+  for(i in 4:ncol(log_data)) {
+    if(grepl('ratio_',colnames(log_data)[i])) {
+      log_data[,i]=log(log_data[,i]+ration_adj)
+    } else {
+      log_data[,i]=log(log_data[,i]+logadj)
+    }
+  }
+  
+  colnames(log_data)[4:ncol(log_data)]=paste('ln',colnames(log_data)[4:ncol(log_data)],sep="")
+  
+  nrow(unique(unstockeddata[,c(1:3)]))
+  nrow(unique(log_data[,c(1:3)]))
+  
+  data=merge(unstockeddata,log_data,by=c('quarter','month','week'))
+  write.csv(data,"unstockeddata.csv",row.names=F)
+  
+  return(data)
   
 }
