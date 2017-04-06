f4f7b01a181f3bc91439a23e54791a3a88c90811
SM Ryu <seungme.ryu@samsung.com>
2017-04-06 20:36:55 +0900

    Add files via upload

---
+++ R/systemmodelfitting.R
@@ -0,0 +1,22 @@
+
+systemmodelfitting=function(){
+  library(reshape2)
+  library(ggplot2)
+  
+  y_hat=with(stockeddata, exp(eval(parse(text=full_equation))))
+  
+  temp=data.frame(time=c(1:length(y_hat),y=stockeddata[,depvar],y_hat=y_hat)
+  temp_melt=melt(temp, id='time')
+  
+  ggplot()+
+    geom_line(aes(x=time, y=value, colour=variable), data=temp_melt, stat="identity") +
+    ggtitle("") +
+    theme(legend.position="right", legend.direction="vertical", legend.title = element_blank()) +
+    labs(fill="") +
+    labs(x='time', y="sales amount") +
+    theme_bw() +
+    theme(axis.line = element_line(size=1, colour = "black"), panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), panel.border = element_blank(),
+          panel.background = element_blank())
+  
+}
