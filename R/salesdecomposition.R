ed4597d60ce5e6b4850f13f95358b22977a191d5
SM Ryu <seungme.ryu@samsung.com>
2017-04-07 15:27:30 +0900

    Update salesdecomposition.R

--- R/salesdecomposition.R
+++ R/salesdecomposition.R
@@ -1,28 +1,21 @@
 
-salesdecomposition=function(){
+salesdecomposition=function(decom_variable){     
   
-  geneqs()
+  y_hat=with(stockeddata, exp(eval(parse(text=resltset$formula))))   
+  n=length(decom_variable)     
+  decompose=NULL   
   
-  dependent<-y
-  decom_variable<-inputvar
+  for(i in 1:n){     
+    temp=stockeddata     
+    temp[,decom_variable[i]]=0     
+    temp1=with(temp,exp(eval(parse(text=resltset$formula))))     
+    temp2=y_hat-temp1     decompose=cbind(decompose,temp2)   
+  }   
   
-  y_hat=with(data, exp(eval(parse(text=full_equation))))
-  n=length(decom_variable)
+  decompose=data.frame(decompose)   
+  decompose$y=exp(stockeddata[,depvar])  
+  colnames(decompose)=c(decom_variable,depvar)   
+  decompose$base=decompose[,depvar]-apply(decompose[,c(1:n)],1,sum)     
   
-  decompose=NULL
-  for(i in 1:n){
-    temp=data
-    temp[,decom_variable[i]]=0
-    temp1=with(temp,exp(eval(parse(text=full_equation))))
-    temp2=y_hat-temp1
-    decompose=cbind(decompose,temp2)
-  }
-  decompose=data.frame(decompose)
-  decompose$y=data[,dependent]
-  colnames(decompose)=c(decom_variable,dependent)
-  decompose$base=decompose[,dependent]-apply(decompose[,c(1:n)],1,sum)
-  
-  return(decompose)
-}
-
+  return(decompose) } 
 
