aad004cac091d3d7368b01a4627f6624c7f3d1b1
SM Ryu <seungme.ryu@samsung.com>
2017-04-07 18:29:42 +0900

    Update grpprice.R

--- R/grpprice.R
+++ R/grpprice.R
@@ -1,34 +1,26 @@
-grpprice<-function(
-  startMonth,
-  endMonth
-){
-    
-    transformed1<-data
-    transformed2<-unstockeddata
-    
-    
-    
-    grpprice<-NULL
-    alltvinputtotal<-sum(transformed1[(transformed1$month>=startMonth & transformed1$month<=endMonth),c('All_TV')])
-    allgrpinputtotal<-sum(transformed1[(transformed1$month>=startMonth & transformed1$month<=endMonth),c('grp_all')])
-    
-    grpprice$pricepergrp <- alltvinputtotal/allgrpinputtotal
-    
-    tvinputtotal<-sum(transformed1[(transformed2$month>=startMonth & transformed1$month<=endMonth),c('tv')])
-    tvgrpinputtotal<-sum(transformed1[(transformed2$month>=startMonth & transformed1$month<=endMonth),c('grp_tv')])
-    grpprice$tvpricepergrp <- tvinputtotal/tvgrpinputtotal
-    
-    catvinputtotal<-sum(transformed1[(transformed2$month>=startMonth & transformed1$month<=endMonth),c('cable_tv')])
-    catvgrpinputtotal<-sum(transformed1[(transformed2$month>=startMonth & transformed1$month<=endMonth),c('grp_ca')])
-    grpprice$catvpricepergrp <- catvinputtotal/catvgrpinputtotal
-    
-    jptvinputtotal<-sum(transformed1[(transformed2$month>=startMonth & transformed1$month<=endMonth),c('jp')])
-    jptvgrpinputtotal<-sum(transformed1[(transformed2$month>=startMonth & transformed1$month<=endMonth),c('grp_jp')])
-    grpprice$jptvpricepergrp <- jptvinputtotal/jptvgrpinputtotal
-
-
-    return(grpprice)
-}
-
-
-
+grpprice<-function(startMonth,endMonth){     
+  
+  grpprice<-NULL   
+  
+  alltvinputtotal<-sum(stockeddata[(stockeddata$month>=startMonth & stockeddata$month<=endMonth),c('All_TV')])   
+  allgrpinputtotal<-sum(stockeddata[(stockeddata$month>=startMonth & stockeddata$month<=endMonth),c('grp_all')])   
+  
+  grpprice$pricepergrp <- alltvinputtotal/allgrpinputtotal     
+  
+  tvinputtotal<-sum(stockeddata[(unstockeddata$month>=startMonth & stockeddata$month<=endMonth),c('tv')])   
+  tvgrpinputtotal<-sum(stockeddata[(unstockeddata$month>=startMonth & stockeddata$month<=endMonth),c('grp_tv')])   
+  
+  grpprice$tvpricepergrp <- tvinputtotal/tvgrpinputtotal
+  
+  catvinputtotal<-sum(stockeddata[(unstockeddata$month>=startMonth & stockeddata$month<=endMonth),c('cable_tv')])   
+  catvgrpinputtotal<-sum(stockeddata[(unstockeddata$month>=startMonth & stockeddata$month<=endMonth),c('grp_ca')])   
+  
+  grpprice$catvpricepergrp <- catvinputtotal/catvgrpinputtotal     
+  
+  jptvinputtotal<-sum(stockeddata[(unstockeddata$month>=startMonth & stockeddata$month<=endMonth),c('jp')])   
+  jptvgrpinputtotal<-sum(stockeddata[(unstockeddata$month>=startMonth & stockeddata$month<=endMonth),c('grp_jp')])   
+  
+  grpprice$jptvpricepergrp <- jptvinputtotal/jptvgrpinputtotal      
+  
+  return(grpprice) 
+} 
