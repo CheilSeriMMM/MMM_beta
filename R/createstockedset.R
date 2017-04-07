bececb16f4f45396b3fd59a94ece9fe760d295bd
SM Ryu <seungme.ryu@samsung.com>
2017-04-07 15:02:16 +0900

    Update createstockedset.R

--- R/createstockedset.R
+++ R/createstockedset.R
@@ -1,26 +1,24 @@
-createstockedset=function(){
-  week_transformed <- unstockeddata 
+createstockedset <- function(carryover) {
   
-  carryrate <- lambda1
+  detach("package:dplyr", unload=TRUE)
+  stockedcand=carryover[carryover$lambda > 0,]
   
-  colnames(week_transformed)
-  target_var <- c(8:17, 31:50)
-  
-  for (i in 1 : length(target_var)){
-    j <- target_var[i]
-    week_transformed[,j] <- filter(week_transformed[,j], filter = carryrate, method = "recursive")
+  for(i in 1:nrow(stockedcand)) {
+    if(stockedcand$media=="digital") {
+      mediavar=digitalvar
+    } else if(stockedcand$media=="atl") {
+      mediavar=atlvar
+    } else {
+      print("Error occured during carryover process! Check that module!!")
+    }
+    idx=colnames(unstockeddata) %in% mediavar
+    unstockeddata[,idx] = filter(unstockeddata[,idx], filter=stockedcand$lambda, method="recursive")
   }
   
-  ATLad_share_stocked<-ddply(week_transformed, .(week), summarise, 
-                             adshare=sum(monitor_tv_saida, monitor_catv_saida, monitor_jp_saida, monitor_radio_saida, na.rm=TRUE)/
-                               sum(monitor_tv_saida, monitor_catv_saida, monitor_jp_saida, monitor_radio_saida,monitor_tv_others_all, monitor_catv_others_all, monitor_jp_others_all, monitor_radio_others_all, na.rm=TRUE))
+  stockeddata=unstockeddata
+  write.csv(stockeddata,"stockeddata.csv",row.names=F)
   
-  week_transformed$ms_ATL <- ATLad_share_stocked$adshare 
+  library(dplyr)
+  return(stockeddata)
   
-  ATLad_ratio_stocked<-ddply(week_transformed, .(week), summarise, adshare=sum(All_TV, radio, na.rm=TRUE)/sum(monitor_tv_others_all, monitor_catv_others_all, monitor_jp_others_all, monitor_radio_others_all, na.rm=TRUE))
-  week_transformed$ATL_ad_ratio_to_comp <- ATLad_ratio_stocked$adshare
-  
-  write.csv(week_transformed, 'data.csv', row.names=F)
-  
-  return(week_transformed)
 }
